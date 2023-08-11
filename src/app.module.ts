494e91d9de84bc226e774ead3adf6257873c8bfd
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-08-11 19:35:42 -0300

    Configure TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.development.local', '.env.development'],
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
     }),
     UserModule,
   ],

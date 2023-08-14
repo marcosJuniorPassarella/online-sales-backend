a7ab6a9539e6f0ed099e3092c42211b46a303455
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-08-14 18:44:42 -0300

    Implement user repository

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,24 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './entity/user.entity';
 import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
   public async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const saltOrRounds = 10;
     const passwordHashed = await hash(createUserDto.password, saltOrRounds);
 
-    const newUser: UserEntity = {
+    return this.userRepository.save({
       ...createUserDto,
-      id: this.users.length + 1,
       password: passwordHashed,
-    };
-    this.users.push(newUser);
-
-    return newUser;
+    });
   }
 
   public async getAllUsers(): Promise<Array<UserEntity>> {
-    return this.users;
+    return this.userRepository.find();
   }
 }

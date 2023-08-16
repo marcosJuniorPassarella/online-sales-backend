584cda6dd72a260e0bc9c857fbf00fc342ff040d
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-08-16 18:13:10 -0300

    Implement state service

--- src/state/state.service.ts
+++ src/state/state.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { StateEntity } from './entities/state.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class StateService {}
+export class StateService {
+  constructor(
+    @InjectRepository(StateEntity)
+    private readonly stateRepository: Repository<StateEntity>,
+  ) {}
+
+  public async getAllStates(): Promise<Array<StateEntity>> {
+    return await this.stateRepository.find();
+  }
+}

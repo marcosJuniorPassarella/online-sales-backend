b368a7c33da345ff61d286ec124c92c93cd5a7d2
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-08-16 18:12:51 -0300

    Implement state controller

--- src/state/state.controller.ts
+++ src/state/state.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { StateService } from './state.service';
+import { StateEntity } from './entities/state.entity';
 
 @Controller('state')
-export class StateController {}
+export class StateController {
+  constructor(private readonly stateService: StateService) {}
+
+  @Get()
+  public async getAllStates(): Promise<Array<StateEntity>> {
+    return await this.stateService.getAllStates();
+  }
+}

2fbda142358cb751cbaacfb4cea3e0c63d6d2019
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-08-16 18:02:12 -0300

    Generated state service

--- src/state/state.module.ts
+++ src/state/state.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StateController } from './state.controller';
+import { StateService } from './state.service';
 
 @Module({
-  controllers: [StateController]
+  controllers: [StateController],
+  providers: [StateService]
 })
 export class StateModule {}

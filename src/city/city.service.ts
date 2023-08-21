73766b08e87da261a7f863105d07cbf88d4b87f5
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-08-21 18:16:29 -0300

    Implement city service using cache strategy

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,20 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { CityEntity } from './entities/city.entity';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Repository } from 'typeorm';
+import { CityEntity } from './entities/city.entity';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
-  public async getAllCitiesByStateId(stateId: number): Promise<Array<CityEntity>> {
-    return await this.cityRepository.find({
+  public async getAllCitiesByStateId(
+    stateId: number,
+  ): Promise<Array<CityEntity>> {
+    const citiesCache: Array<CityEntity> = await this.cacheManager.get(
+      `state_${stateId}`,
+    );
+
+    if (citiesCache) {
+      return citiesCache;
+    }
+
+    const cities = await this.cityRepository.find({
       where: {
         stateId: stateId,
       },
     });
+    await this.cacheManager.set(`state_${stateId}`, cities);
+
+    return cities;
   }
 }

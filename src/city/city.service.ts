afc96cc750718c3e8f3e52ebda6d8b86f57e7828
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-08-21 17:41:42 -0300

    Implement city service

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CityEntity } from './entities/city.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CityService {}
+export class CityService {
+  constructor(
+    @InjectRepository(CityEntity)
+    private readonly cityRepository: Repository<CityEntity>,
+  ) {}
+
+  public async getAllCitiesByStateId(stateId: number): Promise<Array<CityEntity>> {
+    return await this.cityRepository.find({
+      where: {
+        stateId: stateId,
+      },
+    });
+  }
+}

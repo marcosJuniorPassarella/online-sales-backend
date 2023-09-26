cc5004fc5aba82797a2e66238ad13c20bc441a9a
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-09-26 18:33:53 -0300

    Finished implementation custom decorator

--- src/decorators/user-id.decorator.ts
+++ src/decorators/user-id.decorator.ts
@@ -1,7 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { authorizationToLoginPayload } from 'src/utils/base-64-converter';
 
 export const UserId = createParamDecorator((_, ctx: ExecutionContext) => {
   const { authorization } = ctx.switchToHttp().getRequest().headers;
-  console.log('AUTHORIZATION', authorization);
-  return 1;
+  const loginPayload = authorizationToLoginPayload(authorization);
+  return loginPayload?.id;
 });
